415069f42846c4cc39fdf4db749a6b2951c6f4d0
Gaurav Malakar <varuag2002@gmail.com>
2023-02-07 18:02:22 +0530

    shell changed

--- shell.cpp
+++ shell.cpp
@@ -5,47 +5,66 @@
 #include <wait.h>
 #include <fcntl.h>
 #include <signal.h>
+#include <dirent.h>
 
-#define RESET "\x1B[0m"
-#define BOLD "\x1B[1m" // Bold Text Formula...
-#define RED "\033[1;32m"
 
 using namespace std;
+
 #include "global_variables.h"
-#include "execute_single_command.h"
+#include "wildcards.h"
+vector<pid_t> background_processes; // Vector to store the PIDs of background processes
 
 void printPrompt(); // Function to print the prompt
+void check_background_processes(); // Function to check if any background process has finished
 // Function to handle the SIGINT signal (Ctrl+C) and print the prompt after that
-void sig_handler_prompt(int signum)
-{
-    printf("\n");
-    printPrompt();
-}
+void sig_handler_prompt(int signum){ printf("\n"); printPrompt(); }
 // Function to handle the SIGINT signal (Ctrl+C) and not print the prompt after that
-void sig_handler_no_prompt(int signum) { printf("\n"); }
+void sig_handler_no_prompt(int signum){ printf("\n"); }
 
-int main()
-{
-    while (true)
-    {
+
+#include "execute_single_command.h"
+
+
+
+
+
+int main(){
+    while(true){
+        vector ar = background_processes;
         printPrompt();
         signal(SIGINT, sig_handler_prompt);
         string command;
         getline(cin, command);
+        //printf("Input taken: %s\n", command.c_str()); fflush(stdout);
         signal(SIGINT, sig_handler_no_prompt);
-        execute(command);
+        executeSingleCommand(command);
+        fflush(stdout);
     }
-
+    
     return 0;
+
 }
 
 // Function to print the prompt
-void printPrompt()
-{
+void printPrompt(){
+    check_background_processes();
     getcwd(curr_working_dir, sizeof(curr_working_dir));
-    printf("%s", RED);
-    printf("%s", BOLD);
     cout << "our-shell:" << curr_working_dir << "$ ";
-    printf("%s", RESET);
     fflush(stdout);
+}
+
+// Function to check if any background process has finished
+void check_background_processes(){
+    for(int i=0; i<background_processes.size(); i++){
+        if(background_processes[i]==-1) continue; // process already finished (waitpid() was called
+        int status;
+        pid_t pid = waitpid(background_processes[i], &status, WNOHANG);
+        if(pid>0){
+            printf("[%d] Done\n", i+1);
+            fflush(stdout);
+            background_processes[i] = -1;
+        }
+    }
+    while(background_processes.size() && background_processes.back()==-1) 
+        background_processes.pop_back();
 }
